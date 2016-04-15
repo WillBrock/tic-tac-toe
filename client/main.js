8ecb63428671906a88f75024c54a4e81b65a6d64
Will Brock <wpbrock@gmail.com>
2016-04-15 19:45:01 -0400

    Fri

--- client/main.js
+++ client/main.js
@@ -1,24 +1,52 @@
 (function() {
 	"use strict";
 
-	let socket = io();
-	let active_player_name;
+	let socket        = io();
+	let current_turn  = false;
+	let player_letter = 'X';
+	let current_room  = null;
+
+	// Different win combinations
+	let wins = [
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 9],
+		[1, 4, 7],
+		[2, 5, 8],
+		[3, 6, 9],
+		[1, 5, 9],
+		[3, 5, 7]
+	];
 
 	// Submit new player
 	document.querySelector('.player-name-form').addEventListener('submit', (e) => {
 		e.preventDefault();
 		let player_name_input = document.querySelector('.player-name');
-		active_player_name    = player_name_input.value;
 
-		socket.emit('add-player', active_player_name);
+		socket.emit('add-player', player_name_input.value);
 		document.querySelector('.player-name-form').className += " hidden";
 	});
 
-	// Challange a user to a game
-	document.querySelector('.play-button', function() {
-		let player_id = this.getAttribute('data-player-id');
+	// Click a cell on the board
+	document.querySelector('.square').addEventListener('click', function() {
+		// Only continue if it's the current users turn or the cell has been used
+		if(!current_turn || this.getAttribute('data-value')) {
+			return;
+		}
 
-		// Remove the current user and challange user from the awaiting user list
+		let element = document.createElement('span');
+		element.setAttribute('data-value', player_letter);
+		element.innerText(player_letter);
+
+		this.appendChild(element);
+
+		socket.emit('take-turn', {
+			cell : this.getAttribute('data-cell'),
+			room : current_room
+		});
+
+		// Check for a winner
+		let winner = checkWinner();
 	});
 
 	// List of all players wanting to play a game
@@ -40,10 +68,21 @@
 	socket.on('delete-player', (player_id) => {
 		let player_class = player_id.replace(/\/|#/g, '');
 		// Query selector doesn't like / or #
-		let element      = document.querySelector(`.prefix-${player_class}`);
+		let element = document.querySelector(`.prefix-${player_class}`);
 
 		element.parentNode.removeChild(element);
 	});
+
+	socket.on('test', (msg) => {
+		console.log(msg);
+	});
+
+	function challangePlayer() {
+		let player_id = this.getAttribute('data-player-id');
+
+		console.log('challange function');
+		socket.emit('challange-player', player_id);
+	}
 
 	function setAwatingPlayers(players, all = false) {
 		let container = document.querySelector('.waiting-users-container');
@@ -66,7 +105,13 @@
 			name_element.innerText = player.player_name;
 			button_element.setAttribute('data-player-id', player.id);
 
+			button_element.addEventListener('click', challangePlayer);
+
 			container.appendChild(cloned_item);
 		}
+	}
+
+	function checkWinner() {
+
 	}
 }());
--- server/main.js
+++ server/main.js
@@ -3,12 +3,14 @@ module.exports = (io) => {
 
 	let awaiting_players = {};
 	let User             = require(`${__dirname}/models/User`);
+	let client_id        = null;
 
 	io.on('connection', (client) => {
 
 		// Emit players waitng to play
 		client.emit('awaiting-players', awaiting_players);
 
+		// Disconnect the client
 		client.on('disconnect', () => {
 			if(awaiting_players[client.id]) {
 				io.emit('delete-player', client.id);
@@ -23,7 +25,20 @@ module.exports = (io) => {
 				player_name : player_name
 			};
 
-			io.emit('player-added', awaiting_players[client.id]);
+			client.broadcast.emit('player-added', awaiting_players[client.id]);
+		});
+
+		// One player challanges another
+		client.on('challange-player', (accepting_user) => {
+			// Need to create the new game room
+
+			// Emit to the accepting user that someone has challenged them
+			io.sockets.connected[accepting_user].emit('test', 'blah');
+		});
+
+		// A player takes a turn
+		client.on('take-turn', (data) => {
+
 		});
 	});
 
